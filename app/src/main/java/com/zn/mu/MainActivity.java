91720a2bf6b2d18080191ef2952dbef4c703f54c
treesouth <zntreesouth@gmail.com>
2015-01-03 22:52:48 +0800

    init frame , add ripple

--- app/src/main/java/com/zn/mu/MainActivity.java
+++ app/src/main/java/com/zn/mu/MainActivity.java
@@ -1,5 +1,6 @@
 package com.zn.mu;
 
+import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
@@ -8,12 +9,22 @@ import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
-public class MainActivity extends ActionBarActivity {
+import com.zn.mu.activity.RippleActivity;
+
+public class MainActivity extends ActionBarActivity implements AdapterView.OnItemClickListener {
     private DrawerLayout mDrawerLayout;
     private ListView mDrawerList;
     private ActionBarDrawerToggle mDrawerToggle;
+
+    private String[] mDrawerTitles;
+
+    private long exitTime = 0L;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,8 +34,12 @@ public class MainActivity extends ActionBarActivity {
         ActionBar ab = getSupportActionBar();
         ab.setDisplayHomeAsUpEnabled(true);
 
+        mDrawerTitles = getResources().getStringArray(R.array.drawerTitles);
+
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
         mDrawerList = (ListView) findViewById(R.id.navdrawer);
+        mDrawerList.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, mDrawerTitles));
+        mDrawerList.setOnItemClickListener(this);
 
         mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.drawer_open, R.string.drawer_close);
         mDrawerToggle.syncState();
@@ -32,6 +47,15 @@ public class MainActivity extends ActionBarActivity {
         mDrawerLayout.setDrawerListener(mDrawerToggle);
     }
 
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        switch (position) {
+            case 0:
+                startActivity(new Intent(this, RippleActivity.class));
+                break;
+        }
+        mDrawerLayout.closeDrawer(mDrawerList);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -73,4 +97,17 @@ public class MainActivity extends ActionBarActivity {
         super.onConfigurationChanged(newConfig);
         mDrawerToggle.onConfigurationChanged(newConfig);
     }
+
+    //    双击返回退出应用
+    @Override
+    public void onBackPressed() {
+        if ((System.currentTimeMillis() - exitTime) > 2000) {
+            Toast.makeText(this, "再次按返回键退出", Toast.LENGTH_SHORT).show();
+            exitTime = System.currentTimeMillis();
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+
 }
